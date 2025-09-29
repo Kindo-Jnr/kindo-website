878a245ac4fc880b81cb9d102e4c1800236f82ea
kindo <kindojnr58@gmail.com>
2025-09-29 14:48:13 +0000

    project built

--- kindo/src/pages/Home.jsx
+++ kindo/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <section className="h-screen flex flex-col items-center justify-center text-center">
-      <h1 className="text-6xl font-bold text-brand-accent">Kindo</h1>
+      <h1 className="text-8xl font-bold text-brand-accent">Kindo</h1>
       <p className="mt-4 text-lg text-gray-400">
         Creative Developer & Designer
       </p>
